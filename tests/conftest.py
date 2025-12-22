1d655cd1108449eb0ed8af6e7c5ea9b00bcc8e93
fyff <soul.2fast4u@gmail.com>
2025-12-22 22:59:03 +0100

    PM-2-L-3 added dotenv lib config added dataclass lib for simplification for configs added faker lib for random data generation tests refactoring

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass
 
 import pytest
 from dotenv import load_dotenv
@@ -6,11 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+@dataclass(frozen=True)
+class Config:
+    base_url: str
+    login_url: str
+    email: str
+    password: str
+
+
 @pytest.fixture(scope="session")
 def configs():
-    return {
-        "base_url": f"{os.getenv("BASE_URL")}",
-        "login_url": f"{os.getenv("BASE_APP_URL")}/users/sign_in",
-        "email": os.getenv("EMAIL"),
-        "password": os.getenv("PASSWORD"),
-    }
+    return Config(
+        base_url=os.getenv("BASE_URL"),
+        login_url=os.getenv("BASE_APP_URL"),
+        email=os.getenv("EMAIL"),
+        password=os.getenv("PASSWORD")
+    )
--- tests/first_test.py
+++ tests/first_test.py
@@ -1,39 +1,47 @@
 import os
 
+import pytest
+from faker import Faker
 from playwright.sync_api import Page, expect
 
+from tests.conftest import Config
 
-def test_login_with_invalid_creds(page: Page, configs: dict):
+TARGET_PROJECT = "python manufacture"
+
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs: Config):
+    page.goto(configs.login_url)
+    login_user(page, configs.email, configs.password)
+
+
+def test_login_with_invalid_creds(page: Page, configs: Config):
     open_home_page(page)
     page.get_by_text("Log in", exact=True).click()
-    login_user(page, configs["email"], "invalid-password")
+    invalid_password = Faker().password(length=10)
+
+    login_user(page, configs.email, invalid_password)
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
 
 
-def test_search_project_in_company(page: Page, configs: dict):
-    page.goto(configs["login_url"])
-    login_user(page, configs["email"], configs["password"])
+def test_search_project_in_company(page: Page, login):
     # Note for Roma. I have a personal Testomat project, so I need to do an extra proj selection.
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("QA Club Lviv")
-    target_project = "python manufacture"
-    search_project(page, target_project)
+    search_project(page, TARGET_PROJECT)
 
-    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
     # expect(page.locator("ul li h3")).to_have_text(target_project, use_inner_text=True)
 
 
-def test_open_free_project(page: Page, configs: dict):
-    page.goto(configs["login_url"])
-    login_user(page, configs["email"], configs["password"])
+def test_open_free_project(page: Page, login):
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
+    search_project(page, TARGET_PROJECT)
 
-    target_project = "python manufacture"
-    search_project(page, target_project)
-    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_hidden()
     expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
 
 
